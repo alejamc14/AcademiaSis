ccfb41f69c13bc95d4e8ea1458c34f35db98045a
alejamc14 <alejandra.madrid1426@gmail.com>
2024-11-14 13:09:50 -0500

    C

---
+++ AcademiaSistemas/SERVICIO2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SERVICIO2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ AcademiaSistemas/SERVICIO2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SERVICIO2.WebApiApplication" Language="C#" %>
